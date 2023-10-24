dbd5f2e43b28787035dedf16648fea57ff979391
AirbnbEcoPlus <add7bn@gmail.com>
2023-10-24 09:20:53 +0200

    Saisie message d'erreur + saisie epreuve

--- Main.java
+++ Main.java
@@ -2,10 +2,36 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
+        // Variables
+        int competiteurs[];
+        int nombreCompetiteurs;
+        int longeurPiste;
+        int nombreObstacle;
+        int nombreTotalBarres;
+
+        // Mettre un petite affichage pour le bohneur des yeux
+        nombreCompetiteurs = saisieIntBornes(1, 50, "Saisir le nombre de competiteurs : ");
+        longeurPiste = saisieIntBorneMin(0, "Saisir la longueur de la piste : ");
+        nombreObstacle = saisieIntBorneMin(1, "Saisir le nombe d'obstacles : ");
+        nombreTotalBarres = saisieTotalBarres(nombreObstacle);
+
+        competiteurs = new int[nombreCompetiteurs];
 
     }
-    
 
+    /**
+     * Permet la saisie a l'utilisateur du nombre Total de barres en vérifiant la
+     * coherence avec le nombre d'obstacle
+     * 
+     * @author Adrien Faure
+     * @param tab   OUT tableau de valeurs
+     * @param nbVal IN nombre de valeurs a saisir
+     */
+    public static int saisieTotalBarres(int nombreObstacle) {
+        int max = nombreObstacle * 2;
+        int min = nombreObstacle * 4;
+        return saisieIntBornes(min, max, "Saisir le nombre total de barres");
+    }
 
     /**
      * Permet la saisie a l'utilisateur d'un tableau
@@ -37,6 +63,7 @@ class Main {
         Scanner clavier = new Scanner(System.in);
         int rst = clavier.nextInt();
         while (rst < min) {
+            System.out.println("(Valeur minimale " + min + ")");
             System.out.print(msg);
             rst = clavier.nextInt();
         }
@@ -56,6 +83,7 @@ class Main {
         Scanner clavier = new Scanner(System.in);
         int rst = clavier.nextInt();
         while (rst > max) {
+            System.out.println("(Valeur maximale " + max + ")");
             System.out.print(msg);
             rst = clavier.nextInt();
         }
@@ -76,6 +104,7 @@ class Main {
         Scanner clavier = new Scanner(System.in);
         int rst = clavier.nextInt();
         while (rst < min || rst > max) {
+            System.out.println("(Valeur comprise entre " + min + " et " + max + ")");
             System.out.print(msg);
             rst = clavier.nextInt();
         }

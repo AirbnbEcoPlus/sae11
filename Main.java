7c3958deea1302bc8f290ce1806854c8765b55c8
victorjockin <jockinvictor@gmail.com>
2023-10-24 08:21:06 +0200

    espaces ajoutés

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 
 class Main {
-    public static void main(String[] args){
-        System.out.println("J'adore LES COURS DE COM");
+    public static void main(String[] args) {
+        System.out.println("J'adore LES COURS DE COM") ;
     }
 }
